4cc89d3d40463a296286307ee8edec476e67032f
Kuldeep Garg <kuldeepgarg6993@gmail.com>
2026-01-13 16:06:13 +0530

    File updated at 1/13/2026, 4:06:12 PM

--- 4-MedianofTwoSortedArrays/4-MedianofTwoSortedArrays.java
+++ 4-MedianofTwoSortedArrays/4-MedianofTwoSortedArrays.java
@@ -1,28 +1,26 @@
-// Last updated: 12/18/2025, 10:59:02 PM
+// Last updated: 1/13/2026, 4:06:12 PM
 1class Solution {
 2    public double findMedianSortedArrays(int[] nums1, int[] nums2) {
 3        return answer(nums1,nums2);
 4    }
-5    public static double answer(int[] nums1,int[] nums2){
-6        int n = nums1.length;
-7        int m = nums2.length;
+5    public static double answer(int[] a, int [] b){
+6        int n = a.length;
+7        int m = b.length;
 8        int[] arr = new int[n+m];
-9
-10        int c=0;
-11        for(int i=0;i<n;i++){
-12            arr[c++] = nums1[i];
-13        }
-14
-15        for(int i=0;i<m;i++){
-16            arr[c++] = nums2[i];
-17        }
-18        Arrays.sort(arr);
-19        
-20        int l = m+n;
-21        if(arr.length%2 != 0){
-22            return arr[l/2];
-23        }else{
-24            return (arr[l/2-1] + arr[l/2])/2.0;
-25        }
-26    }
-27}
+9        int c=0;
+10        for(int i=0 ;i<n ;i++){
+11            arr[c++] = a[i];
+12        }
+13        for(int i=0 ;i<m ;i++){
+14            arr[c++] = b[i];
+15        }
+16        Arrays.sort(arr);
+17        int l = arr.length;
+18        if(l % 2 != 0){
+19            return arr[l/2];
+20        }else{
+21            return (arr[l/2-1]+ arr[l/2])/2.0;
+22        }
+23
+24    }
+25}
