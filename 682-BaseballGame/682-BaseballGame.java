0f9729443d3a5de1d1e519bb6bebc7d3cce83dfc
Kuldeep Garg <kuldeepgarg6993@gmail.com>
2026-01-19 15:59:49 +0530

    File updated at 1/19/2026, 3:59:48 PM

--- 682-BaseballGame/682-BaseballGame.java
+++ 682-BaseballGame/682-BaseballGame.java
@@ -1,32 +1,32 @@
-// Last updated: 8/9/2025, 1:42:35 PM
-class Solution {
-    public int calPoints(String[] operations) {
-        return Baseball_Game(operations);
-    }
-    public static int Baseball_Game(String[] operations){
-        Stack<Integer> st = new Stack<>();
-        int sum=0;
-        for(int i=0;i<operations.length;i++){
-            String ele   = operations[i];
-            if (ele.equals("+")) {
-                int a = st.pop();
-                int b = st.pop();
-                st.push(b);
-                st.push(a);
-                st.push(a + b);
-            } else if(ele.equals("D")){
-                st.push(2*st.peek());
-            }else if(ele.equals("C")){
-                st.pop();
-            }else{
-                st.push(Integer.parseInt(ele));
-            }
-
-        }
-        while(!st.isEmpty()){
-            sum+=st.pop();
-        }
-        return sum;
-
-    }
-}
+// Last updated: 1/19/2026, 3:59:48 PM
+1class Solution {
+2    public int calPoints(String[] operations) {
+3        return Baseball_Game(operations);
+4    }
+5    public static int Baseball_Game(String[] operations){
+6        Stack<Integer> st = new Stack<>();
+7        int sum=0;
+8        for(int i=0;i<operations.length;i++){
+9            String ele   = operations[i];
+10            if (ele.equals("+")) {
+11                int a = st.pop();
+12                int b = st.pop();
+13                st.push(b);
+14                st.push(a);
+15                st.push(a + b);
+16            } else if(ele.equals("D")){
+17                st.push(2*st.peek());
+18            }else if(ele.equals("C")){
+19                st.pop();
+20            }else{
+21                st.push(Integer.parseInt(ele));
+22            }
+23
+24        }
+25        while(!st.isEmpty()){
+26            sum+=st.pop();
+27        }
+28        return sum;
+29
+30    }
+31}
