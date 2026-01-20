2fd33c336b17d0fd3d36b24478eb836845219c87
Kuldeep Garg <kuldeepgarg6993@gmail.com>
2026-01-20 14:33:15 +0530

    File updated at 1/20/2026, 2:33:15 PM

--- 844-BackspaceStringCompare/844-BackspaceStringCompare.java
+++ 844-BackspaceStringCompare/844-BackspaceStringCompare.java
@@ -1,21 +1,27 @@
-// Last updated: 1/20/2026, 2:32:38 PM
+// Last updated: 1/20/2026, 2:33:15 PM
 1class Solution {
 2    public boolean backspaceCompare(String s, String t) {
-3        Stack<Character> s1  = Backspace_len(s);
-4        Stack<Character> s2 = Backspace_len(t);
-5        return s1.equals(s2);
-6    }
-7    public static Stack<Character> Backspace_len(String s){
-8        int n = s.length();
-9        Stack<Character> st = new Stack<>();
-10        for(int i=0;i<n;i++){
-11            char ch = s.charAt(i);
-12            if(!st.isEmpty() && ch == '#'){
-13                st.pop();
-14            }else if(ch != '#'){
-15                st.push(ch);
-16            }
-17        }
-18        return st;
-19    }
-20}
+3        //get the content of string after perform backspace operation
+4        Stack<Character> a = get_Stack(s);
+5        Stack<Character> b = get_Stack(t);
+6
+7        return a.equals(b);
+8    }
+9
+10    public Stack<Character> get_Stack(String s){
+11        int n = s.length();
+12        Stack<Character> st = new Stack<>();
+13
+14        for(int i=0 ;i<n ;i++){
+15            char ch = s.charAt(i);
+16            if(!st.isEmpty() && ch == '#'){
+17                st.pop();
+18            }
+19            if(ch != '#'){
+20                st.push(ch);
+21            }
+22        }
+23
+24        return st;
+25    }
+26}
