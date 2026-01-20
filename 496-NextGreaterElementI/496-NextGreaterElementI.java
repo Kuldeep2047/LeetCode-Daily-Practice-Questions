4b0f39294152f85ce528dac5c4892c24fb639fdf
Kuldeep Garg <kuldeepgarg6993@gmail.com>
2026-01-20 14:53:45 +0530

    File updated at 1/20/2026, 2:53:44 PM

--- 496-NextGreaterElementI/496-NextGreaterElementI.java
+++ 496-NextGreaterElementI/496-NextGreaterElementI.java
@@ -1,4 +1,4 @@
-// Last updated: 1/20/2026, 2:47:19 PM
+// Last updated: 1/20/2026, 2:53:44 PM
 1class Solution {
 2    public int[] nextGreaterElement(int[] nums1, int[] nums2) {
 3        int[] nge = nextGreater(nums2);
@@ -14,18 +14,24 @@
 13        
 14    }
 15
-16    public int[] nextGreater(int[] nums2){
-17        int n2 = nums2.length;
-18        int[] nge = new int[n2];
-19        Stack<Integer> st = new Stack<>();
-20        for(int i=n2-1; i>=0; i--) {
-21            while(!st.isEmpty() && st.peek() <= nums2[i]) {
+16    public int[] nextGreater(int[] arr){
+17        int n = arr.length;
+18        Stack<Integer> st = new Stack<>();
+19        int[] nge = new int[n];
+20        for(int i=n-1 ;i>=0 ;i--){
+21            while(!st.isEmpty() && st.peek() < arr[i]){
 22                st.pop();
 23            }
-24            if(st.isEmpty()) nge[i] = -1;
-25            else nge[i] = st.peek();
-26            st.push(nums2[i]);
-27        }
-28        return nge;
-29    }
-30}
+24            if(st.isEmpty()){
+25                nge[i] = -1;
+26            }
+27            else{
+28                nge[i] = st.peek();
+29            }
+30            st.push(arr[i]);
+31        }
+32        return nge;
+33    }
+34
+35    
+36}
