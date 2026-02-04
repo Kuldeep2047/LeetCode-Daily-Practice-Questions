fdbfc4ece273a1c77701a71a0d50f2356796f8e9
Kuldeep Garg <kuldeepgarg6993@gmail.com>
2026-02-04 22:36:12 +0530

    File updated at 2/4/2026, 10:36:11 PM

--- 3112-MinimumTimetoVisitDisappearingNodes/3112-MinimumTimetoVisitDisappearingNodes.java
+++ 3112-MinimumTimetoVisitDisappearingNodes/3112-MinimumTimetoVisitDisappearingNodes.java
@@ -1,55 +1,56 @@
-// Last updated: 1/17/2026, 9:41:13 PM
+// Last updated: 2/4/2026, 10:36:11 PM
 1class Solution {
 2    public int[] minimumTime(int n, int[][] edges, int[] disappear) {
 3        return answer(n, edges, disappear);
 4    }
-5    public int[] answer(int n, int[][] edges, int[] disappear){
-6        //create graph
-7        List<List<int[]>> ll = new ArrayList<>();
-8        for(int i=0 ;i<n ;i++){
-9            ll.add(new ArrayList<>());
-10        }
-11
-12        for(int[] edg : edges){
-13            ll.get(edg[0]).add(new int[]{edg[1], edg[2]});
-14            ll.get(edg[1]).add(new int[]{edg[0], edg[2]});
-15        }
-16
-17        int[] ans = new int[n];
-18        Arrays.fill(ans, 9999999);
-19        boolean[] visited = new boolean[n];
-20        PriorityQueue<int[]> pq = new PriorityQueue<>((a,b)-> a[1] - b[1]);
-21        pq.add(new int[]{0,0});
-22        ans[0] = 0;
-23
-24        while(!pq.isEmpty()){
-25            //remove
-26            int[] r= pq.poll();
-27
-28            //ignore if visited
-29            if(visited[r[0]]){
-30                continue;
-31            }
-32
-33            // mark visited
-34            visited[r[0]] = true;
-35
-36            //add nbrs
-37            for(int[] nbrs : ll.get(r[0])){
-38                int dis = r[1] + nbrs[1];
-39                if(dis< disappear[nbrs[0]] && dis < ans[nbrs[0]]){
-40                    ans[nbrs[0]] = dis;
-41                    pq.add(new int[]{nbrs[0], dis});
-42                }
-43            }
-44        }
-45
-46        for(int i=0 ;i<n ;i++){
-47            if(ans[i] == 9999999){
-48                ans[i] = -1;
-49            }
-50        }
-51
-52        return ans;
-53    }
-54}
+5    
+6    public int[] answer(int n, int[][] edges, int[] disappear){
+7        //create graph
+8        List<List<int[]>> ll = new ArrayList<>();
+9        for(int i=0 ;i<n ;i++){
+10            ll.add(new ArrayList<>());
+11        }
+12
+13        for(int[] edg : edges){
+14            ll.get(edg[0]).add(new int[]{edg[1], edg[2]});
+15            ll.get(edg[1]).add(new int[]{edg[0], edg[2]});
+16        }
+17
+18        int[] ans = new int[n];
+19        Arrays.fill(ans, 9999999);
+20        boolean[] visited = new boolean[n];
+21        PriorityQueue<int[]> pq = new PriorityQueue<>((a,b)-> a[1] - b[1]);
+22        pq.add(new int[]{0,0});
+23        ans[0] = 0;
+24
+25        while(!pq.isEmpty()){
+26            //remove
+27            int[] r= pq.poll();
+28
+29            //ignore if visited
+30            if(visited[r[0]]){
+31                continue;
+32            }
+33
+34            // mark visited
+35            visited[r[0]] = true;
+36
+37            //add nbrs
+38            for(int[] nbrs : ll.get(r[0])){
+39                int dis = r[1] + nbrs[1];
+40                if(dis< disappear[nbrs[0]] && dis < ans[nbrs[0]]){
+41                    ans[nbrs[0]] = dis;
+42                    pq.add(new int[]{nbrs[0], dis});
+43                }
+44            }
+45        }
+46
+47        for(int i=0 ;i<n ;i++){
+48            if(ans[i] == 9999999){
+49                ans[i] = -1;
+50            }
+51        }
+52
+53        return ans;
+54    }
+55}
