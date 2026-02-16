67b83004549e4dfd2d282012ec3619db04442dc3
Kuldeep Garg <kuldeepgarg6993@gmail.com>
2026-02-17 00:46:12 +0530

    File updated at 2/17/2026, 12:46:12 AM

--- 1325-DeleteLeavesWithaGivenValue/1325-DeleteLeavesWithaGivenValue.java
+++ 1325-DeleteLeavesWithaGivenValue/1325-DeleteLeavesWithaGivenValue.java
@@ -1,36 +1,36 @@
-// Last updated: 8/27/2025, 10:35:33 PM
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
-class Solution {
-    public TreeNode removeLeafNodes(TreeNode root, int target) {
-        return answer(root,target);
-        // return root;
-    }
-    public TreeNode answer(TreeNode root,int target){
-        if(root == null){
-            return null;
-        }
-        root.left = answer(root.left,target);
-        root.right = answer(root.right,target);
-        if(root.val == target){
-            if(root.left  == null && root.right==null){
-                return null;
-            }
-        }
-        return root;
-
-    }
-}
+// Last updated: 2/17/2026, 12:46:12 AM
+1/**
+2 * Definition for a binary tree node.
+3 * public class TreeNode {
+4 *     int val;
+5 *     TreeNode left;
+6 *     TreeNode right;
+7 *     TreeNode() {}
+8 *     TreeNode(int val) { this.val = val; }
+9 *     TreeNode(int val, TreeNode left, TreeNode right) {
+10 *         this.val = val;
+11 *         this.left = left;
+12 *         this.right = right;
+13 *     }
+14 * }
+15 */
+16class Solution {
+17    public TreeNode removeLeafNodes(TreeNode root, int target) {
+18        return answer(root,target);
+19        // return root;
+20    }
+21    public TreeNode answer(TreeNode root,int target){
+22        if(root == null){
+23            return null;
+24        }
+25        root.left = answer(root.left,target);
+26        root.right = answer(root.right,target);
+27        if(root.val == target){
+28            if(root.left  == null && root.right==null){
+29                return null;
+30            }
+31        }
+32        return root;
+33
+34    }
+35}
