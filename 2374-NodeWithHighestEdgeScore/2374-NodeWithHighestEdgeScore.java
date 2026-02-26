523d271fe579649fb6cdf43d60f86f233122a429
Kuldeep Garg <kuldeepgarg6993@gmail.com>
2026-02-26 10:37:35 +0530

    File updated at 2/26/2026, 10:37:36 AM

--- 2374-NodeWithHighestEdgeScore/2374-NodeWithHighestEdgeScore.java
+++ 2374-NodeWithHighestEdgeScore/2374-NodeWithHighestEdgeScore.java
@@ -1,30 +1,34 @@
-// Last updated: 2/24/2026, 12:59:00 PM
+// Last updated: 2/26/2026, 10:37:36 AM
 1class Solution {
 2    public int edgeScore(int[] edges) {
 3        HashMap<Integer, List<Integer>> map = new HashMap<>();
 4        for(int i=0; i<edges.length; i++) {
-5            if(!map.containsKey(edges[i])) {
-6                map.put(edges[i], new ArrayList<>());
-7            }
-8
-9            map.get(edges[i]).add(i);
-10        }
-11
-12        long ans = 0;
-13        long max = 0;
-14        
-15        for(int key: map.keySet()) {
-16            long score = 0;
-17
-18            for(int val: map.get(key)){
-19                score = score + val;
-20            }
-21            if(score>max || (score==max && key< ans)) {
-22                max = score;
-23                ans = key;
+5            map.put(i, new ArrayList<>());
+6        }
+7            // if(!map.containsKey(edges[i])) {
+8            //     map.put(edges[i], new ArrayList<>());
+9            // }
+10
+11            // map.get(edges[i]).add(i);
+12        for(int i=0 ;i<edges.length ;i++){
+13            map.get(edges[i]).add(i);
+14        }
+15
+16        long ans = 0;
+17        long max = 0;
+18        
+19        for(int key: map.keySet()) {
+20            long score = 0;
+21
+22            for(int val: map.get(key)){
+23                score = score + val;
 24            }
-25        }
-26
-27        return (int)ans;
-28    }
-29}
+25            if(score>max || (score>=max && key< ans)) {
+26                max = score;
+27                ans = key;
+28            }
+29        }
+30
+31        return (int)ans;
+32    }
+33}
