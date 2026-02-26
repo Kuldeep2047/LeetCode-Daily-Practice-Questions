ea1aea88f670ece78ec7ee8ac97203132310d05a
Kuldeep Garg <kuldeepgarg6993@gmail.com>
2026-02-26 11:22:47 +0530

    File updated at 2/26/2026, 11:22:47 AM

--- 3112-MinimumTimetoVisitDisappearingNodes/3112-MinimumTimetoVisitDisappearingNodes.java
+++ 3112-MinimumTimetoVisitDisappearingNodes/3112-MinimumTimetoVisitDisappearingNodes.java
@@ -1,4 +1,4 @@
-// Last updated: 2/4/2026, 10:36:11 PM
+// Last updated: 2/26/2026, 11:22:47 AM
 1class Solution {
 2    public int[] minimumTime(int n, int[][] edges, int[] disappear) {
 3        return answer(n, edges, disappear);
@@ -18,39 +18,38 @@
 17
 18        int[] ans = new int[n];
 19        Arrays.fill(ans, 9999999);
-20        boolean[] visited = new boolean[n];
-21        PriorityQueue<int[]> pq = new PriorityQueue<>((a,b)-> a[1] - b[1]);
-22        pq.add(new int[]{0,0});
-23        ans[0] = 0;
-24
-25        while(!pq.isEmpty()){
-26            //remove
-27            int[] r= pq.poll();
-28
-29            //ignore if visited
-30            if(visited[r[0]]){
+20        // boolean[] visited = new boolean[n];
+21        HashSet<Integer> visited = new HashSet<>();
+22        PriorityQueue<int[]> pq = new PriorityQueue<>((a,b)-> a[1] - b[1]);
+23        pq.add(new int[]{0,0});
+24        ans[0] = 0;
+25
+26        while(!pq.isEmpty()){
+27            //remove
+28            int[] r= pq.poll();
+29
+30            if(visited.contains(r[0])){
 31                continue;
 32            }
 33
-34            // mark visited
-35            visited[r[0]] = true;
-36
-37            //add nbrs
-38            for(int[] nbrs : ll.get(r[0])){
-39                int dis = r[1] + nbrs[1];
-40                if(dis< disappear[nbrs[0]] && dis < ans[nbrs[0]]){
-41                    ans[nbrs[0]] = dis;
-42                    pq.add(new int[]{nbrs[0], dis});
-43                }
-44            }
-45        }
-46
-47        for(int i=0 ;i<n ;i++){
-48            if(ans[i] == 9999999){
-49                ans[i] = -1;
-50            }
-51        }
-52
-53        return ans;
-54    }
-55}
+34            visited.add(r[0]);
+35
+36            //add nbrs
+37            for(int[] nbrs : ll.get(r[0])){
+38                int dis = r[1] + nbrs[1];
+39                if(dis< disappear[nbrs[0]] && dis < ans[nbrs[0]]){
+40                    ans[nbrs[0]] = dis;
+41                    pq.add(new int[]{nbrs[0], dis});
+42                }
+43            }
+44        }
+45
+46        for(int i=0 ;i<n ;i++){
+47            if(ans[i] == 9999999){
+48                ans[i] = -1;
+49            }
+50        }
+51
+52        return ans;
+53    }
+54}
