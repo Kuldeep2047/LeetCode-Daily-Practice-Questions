f79e9718266ec89341c4c985f94bc324bd72e5c5
Kuldeep Garg <kuldeepgarg6993@gmail.com>
2026-03-07 22:53:50 +0530

    File updated at 3/7/2026, 10:53:50 PM

--- 968-BinaryTreeCameras/968-BinaryTreeCameras.java
+++ 968-BinaryTreeCameras/968-BinaryTreeCameras.java
@@ -1,44 +1,44 @@
-// Last updated: 8/13/2025, 11:34:04 AM
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
-class Solution {
-    int Camera=0;
-    public int minCameraCover(TreeNode root) {
-        int c= minCamera(root);
-        if(c==-1) {
-            Camera++;
-        }
-        return Camera;
-        
-    }
-    public int minCamera(TreeNode root) {
-        if(root == null) {
-            return 0;
-        }
-        int left = minCamera(root.left);
-        int right = minCamera(root.right);
-        if(left == -1 || right ==-1) {	//is node per camera ki need hai
-            Camera++;
-            return 1;	//camera setup kiya hai is node per
-        }
-        else if(left == 1 || right == 1) {	//inme se koi ek ke pass ya dono ke pass camera hai or ek ke pass camera hai and dusra wala covered hai
-            return 0;	//iska mtlb me covered hu
-        }else {
-            return -1;	//need a Camera
-        }
-        
-    }
-}
+// Last updated: 3/7/2026, 10:53:50 PM
+1/**
+2 * Definition for a binary tree node.
+3 * public class TreeNode {
+4 *     int val;
+5 *     TreeNode left;
+6 *     TreeNode right;
+7 *     TreeNode() {}
+8 *     TreeNode(int val) { this.val = val; }
+9 *     TreeNode(int val, TreeNode left, TreeNode right) {
+10 *         this.val = val;
+11 *         this.left = left;
+12 *         this.right = right;
+13 *     }
+14 * }
+15 */
+16class Solution {
+17    int Camera=0;
+18    public int minCameraCover(TreeNode root) {
+19        int c= minCamera(root);
+20        if(c==-1) {
+21            Camera++;
+22        }
+23        return Camera;
+24        
+25    }
+26    public int minCamera(TreeNode root) {
+27        if(root == null) {
+28            return 0;
+29        }
+30        int left = minCamera(root.left);
+31        int right = minCamera(root.right);
+32        if(left == -1 || right ==-1) {	//is node per camera ki need hai
+33            Camera++;
+34            return 1;	//camera setup kiya hai is node per
+35        }
+36        else if(left == 1 || right == 1) {	//inme se koi ek ke pass ya dono ke pass camera hai or ek ke pass camera hai and dusra wala covered hai
+37            return 0;	//iska mtlb me covered hu
+38        }else {
+39            return -1;	//need a Camera
+40        }
+41        
+42    }
+43}
