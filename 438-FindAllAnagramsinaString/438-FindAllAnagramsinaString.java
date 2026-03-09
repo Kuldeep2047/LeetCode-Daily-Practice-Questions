9184f5dcb6ffc1b58899b637cf475d08ccb8c952
Kuldeep Garg <kuldeepgarg6993@gmail.com>
2026-03-09 15:35:42 +0530

    File updated at 3/9/2026, 3:35:41 PM

--- 438-FindAllAnagramsinaString/438-FindAllAnagramsinaString.java
+++ 438-FindAllAnagramsinaString/438-FindAllAnagramsinaString.java
@@ -1,42 +1,42 @@
-// Last updated: 9/30/2025, 8:39:35 PM
-class Solution {
-    public List<Integer> findAnagrams(String s, String p) {
-        return Anagrams(s,p);
-    }
-    public static List<Integer> Anagrams(String s , String p){
-        int[] arr = new int[26];
-        int m = p.length();
-        int n = s.length();
-        List<Integer> ll = new ArrayList<>();
-        for(int i=0;i<m;i++){
-            arr[p.charAt(i)-'a']++;
-        }
-        int si =0;
-        int ei =0;
-        while(ei<n){
-            arr[s.charAt(ei)-'a']--;
-
-            while(ei-si+1 == m && si<n){
-                if(isZero(arr)){
-                    ll.add(si);
-                }
-                arr[s.charAt(si)-'a']++;
-                si++;
-            }
-
-            ei++;
-
-        }
-        return ll;
-
-    }
-
-    public static boolean isZero(int[] arr){
-        for(int a:arr){
-            if(a != 0){
-                return false;
-            }
-        }
-        return true;
-    }
-}
+// Last updated: 3/9/2026, 3:35:41 PM
+1class Solution {
+2    public List<Integer> findAnagrams(String s, String p) {
+3        return Anagrams(s,p);
+4    }
+5    public static List<Integer> Anagrams(String s , String p){
+6        int n = s.length();
+7        int m = p.length();
+8        List<Integer> ll = new ArrayList<>();
+9        
+10        int[] freq = new int[26];
+11        for(int i=0 ;i<m ;i++){
+12            freq[p.charAt(i) - 'a']++;
+13        }
+14
+15        int si =0;
+16        int ei = 0;
+17        while(ei < n){
+18            freq[s.charAt(ei)-'a']--;
+19
+20            while(ei -si +1 == m && si<n){
+21                if(isAns(freq)){
+22                    ll.add(si);
+23                }
+24                freq[s.charAt(si)-'a']++;
+25                si++;
+26            }
+27            ei++;
+28        }
+29        return ll;
+30
+31    }
+32
+33    public static boolean isAns(int[] arr){
+34        for(int a:arr){
+35            if(a != 0){
+36                return false;
+37            }
+38        }
+39        return true;
+40    }
+41}
