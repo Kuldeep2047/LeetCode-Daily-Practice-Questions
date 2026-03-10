ad1eecbe569502a7fcc73d21d65a551b3d6c1703
Kuldeep Garg <kuldeepgarg6993@gmail.com>
2026-03-10 15:59:40 +0530

    File updated at 3/10/2026, 3:59:39 PM

--- 28-FindtheIndexoftheFirstOccurrenceinaString/28-FindtheIndexoftheFirstOccurrenceinaString.java
+++ 28-FindtheIndexoftheFirstOccurrenceinaString/28-FindtheIndexoftheFirstOccurrenceinaString.java
@@ -1,4 +1,4 @@
-// Last updated: 1/13/2026, 2:14:55 PM
+// Last updated: 3/10/2026, 3:59:39 PM
 1class Solution {
 2    public int strStr(String haystack, String needle) {
 3        return answer(haystack, needle);
@@ -8,7 +8,7 @@
 7        int n = s.length();
 8        int m = sub.length();
 9
-10        // int idx =-1;
+10        
 11        for(int i=0 ;i<=n - m ;i++){
 12            if(sub.equals(s.substring(i,i+m))){
 13                return i;
