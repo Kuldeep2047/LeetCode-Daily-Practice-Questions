0d359cff6abbe3a3af973aa58746b6116fb38c7d
Kuldeep Garg <kuldeepgarg6993@gmail.com>
2026-03-10 16:05:15 +0530

    File updated at 3/10/2026, 4:05:15 PM

--- 443-StringCompression/443-StringCompression.java
+++ 443-StringCompression/443-StringCompression.java
@@ -1,30 +1,30 @@
-// Last updated: 8/9/2025, 1:43:17 PM
-class Solution {
-    public int compress(char[] chars) {
-        return string_compression(chars);
-    }
-    public static int string_compression(char[] chars){
-        
-        String ans="";
-        int i=0;
-        while(i<chars.length){
-            int count=0;
-            char ch = chars[i];
-            while(i<chars.length && ch==chars[i]){
-                count++;
-                i++;
-            }
-            if(count>0){
-                ans += ch;
-                if(count>1){
-                    ans += count;
-                }
-            }
-        }
-
-        for (int j = 0; j < ans.length(); j++) {
-            chars[j] = ans.charAt(j);
-        }
-        return ans.length();
-    }
-}
+// Last updated: 3/10/2026, 4:05:15 PM
+1class Solution {
+2    public int compress(char[] chars) {
+3        return string_compression(chars);
+4    }
+5    public static int string_compression(char[] chars){
+6        
+7        String ans="";
+8        int i=0;
+9        while(i<chars.length){
+10            int count=0;
+11            char ch = chars[i];
+12            while(i<chars.length && ch==chars[i]){
+13                count++;
+14                i++;
+15            }
+16            if(count>0){
+17                ans += ch;
+18                if(count>1){
+19                    ans += count;
+20                }
+21            }
+22        }
+23
+24        for (int j = 0; j < ans.length(); j++) {
+25            chars[j] = ans.charAt(j);
+26        }
+27        return ans.length();
+28    }
+29}
