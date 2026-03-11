3c02e066a38565fcdcc3e5362d12a32defe7c627
Kuldeep Garg <kuldeepgarg6993@gmail.com>
2026-03-11 14:22:19 +0530

    File updated at 3/11/2026, 2:22:17 PM

--- 1091-ShortestPathinBinaryMatrix/1091-ShortestPathinBinaryMatrix.java
+++ 1091-ShortestPathinBinaryMatrix/1091-ShortestPathinBinaryMatrix.java
@@ -1,4 +1,4 @@
-// Last updated: 12/30/2025, 1:08:29 PM
+// Last updated: 3/11/2026, 2:22:17 PM
 1class Solution {
 2    public int shortestPathBinaryMatrix(int[][] grid) {
 3        return bfs(grid);
@@ -13,34 +13,37 @@
 12
 13        int[][] dir = {{0,1},{1,1}, {1,0}, {1,-1},{0,-1}, {-1,-1}, {-1,0}, {-1,1}};
 14        Queue<int[]> q= new LinkedList<>();
-15        boolean[][] visited = new boolean[grid.length][grid[0].length];
-16        visited[0][0] = true;
-17
-18        q.add(new int[]{0,0,1});
-19
-20        while(!q.isEmpty()){
-21            int[] r= q.poll();
-22
-23            if(r[0] == n-1 && r[1] == n-1){
-24                return r[2];
-25            }
-26
-27            for(int[] d : dir){
-28                int cr = r[0] + d[0];
-29                int cc = r[1] + d[1];
-30
-31                if(cr<0 || cc<0 || cr>=n || cc>=n || visited[cr][cc] || grid[cr][cc] == 1){
-32                    continue;
-33                }
-34                visited[cr][cc] = true;
-35                q.add(new int[]{cr, cc, r[2]+1});
-36            }
-37        }
-38        return -1;
-39
-40
-41
+15
+16        boolean[][] visited = new boolean[grid.length][grid[0].length];
+17        visited[0][0] = true;
+18
+19        q.add(new int[]{0,0,1});
+20
+21        while(!q.isEmpty()){
+22            int[] r= q.poll();
+23
+24            //check destination here
+25            if(r[0] == n-1 && r[1] == n-1){
+26                return r[2];
+27            }
+28
+29            //add neighbours 
+30            for(int[] d : dir){
+31                int cr = r[0] + d[0];
+32                int cc = r[1] + d[1];
+33                
+34                if(cr<0 || cc<0 || cr>=n || cc>=n || visited[cr][cc] || grid[cr][cc] == 1){
+35                    continue;
+36                }
+37                visited[cr][cc] = true;
+38                q.add(new int[]{cr, cc, r[2]+1});
+39            }
+40        }
+41        return -1;
 42
 43
-44    }
-45}
+44
+45
+46
+47    }
+48}
