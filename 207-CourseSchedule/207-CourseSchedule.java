7e6c9ba242119e0507c714d15e4ffa6b661a11f0
Kuldeep Garg <kuldeepgarg6993@gmail.com>
2026-03-11 16:03:54 +0530

    File updated at 3/11/2026, 4:03:52 PM

--- 207-CourseSchedule/207-CourseSchedule.java
+++ 207-CourseSchedule/207-CourseSchedule.java
@@ -1,51 +1,43 @@
-// Last updated: 1/2/2026, 12:39:39 AM
+// Last updated: 3/11/2026, 4:03:52 PM
 1class Solution {
-2    public boolean canFinish(int numCourses, int[][] prerequisites) {
-3        return topological_sort(numCourses, prerequisites);
+2    public int[] findRedundantConnection(int[][] edges) {
+3        return answer(edges);
 4    }
-5
-6    public boolean topological_sort(int V, int[][] edges){
-7        HashMap<Integer, ArrayList<Integer>> map = new HashMap<>();
-8        
-9        for(int i=0 ;i<V ;i++){
-10            map.put(i, new ArrayList<>());
-11        }
-12        
-13        for(int i=0 ;i<edges.length ;i++){
-14            map.get(edges[i][0]).add(edges[i][1]);
-15        }
-16        
-17        //get indegree
-18        int[] in = new int[V];
-19        
-20        for(int key : map.keySet()){
-21            for(int i : map.get(key)){
-22                in[i]++;
-23            }
-24        }
-25        
-26        Queue<Integer> q= new LinkedList<>();
-27        ArrayList<Integer> ll = new ArrayList<>();
-28        
-29        for(int i=0 ;i<in.length ;i++){
-30            if(in[i] == 0){
-31                q.add(i);
-32            }
-33        }
-34        
-35        while(!q.isEmpty()){
-36            int r = q.poll();
-37            
-38            ll.add(r);
-39            for(int nbrs : map.get(r)){
-40                in[nbrs]--;
-41                
-42                if(in[nbrs] == 0){
-43                    q.add(nbrs);
-44                }
-45            }
-46        }
-47        
-48        return ll.size() == V;
-49    }
-50}
+5    public int[] answer(int[][] edges){
+6        int n = edges.length;
+7        HashMap<Integer, List<Integer>> map = new HashMap<>();
+8        for(int i=1 ;i<=n ;i++){
+9            map.put(i, new ArrayList<>());
+10        }
+11
+12        for(int[] arr : edges){
+13            int u = arr[0];
+14            int v = arr[1];
+15
+16            Queue<Integer> q = new LinkedList<>();
+17            HashSet<Integer> visited = new HashSet<>();
+18            q.add(u);
+19            while(!q.isEmpty()){
+20                int r = q.poll();
+21                if(visited.contains(r)){
+22                    continue;
+23                }
+24                visited.add(r);
+25                for(int nbrs : map.get(r)){
+26                    if(!visited.contains(nbrs)){
+27                        q.add(nbrs);
+28                    }
+29                }
+30            }
+31            if(visited.contains(v)){
+32                return arr;
+33            }
+34
+35
+36            map.get(u).add(v);
+37            map.get(v).add(u);
+38        }
+39        return new int[]{-1,-1};
+40
+41    }
+42}
