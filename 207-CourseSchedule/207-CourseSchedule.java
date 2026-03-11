b9502633194457f1e1b5cff335936f308ff0a611
Kuldeep Garg <kuldeepgarg6993@gmail.com>
2026-03-11 16:36:45 +0530

    File updated at 3/11/2026, 4:36:44 PM

--- 207-CourseSchedule/207-CourseSchedule.java
+++ 207-CourseSchedule/207-CourseSchedule.java
@@ -1,43 +1,25 @@
-// Last updated: 3/11/2026, 4:03:52 PM
+// Last updated: 3/11/2026, 4:36:44 PM
 1class Solution {
-2    public int[] findRedundantConnection(int[][] edges) {
-3        return answer(edges);
-4    }
-5    public int[] answer(int[][] edges){
-6        int n = edges.length;
-7        HashMap<Integer, List<Integer>> map = new HashMap<>();
-8        for(int i=1 ;i<=n ;i++){
-9            map.put(i, new ArrayList<>());
+2    public int[][] floodFill(int[][] image, int sr, int sc, int color) {
+3        int temp = image[sr][sc];
+4        answer(image, sr, sc, color, temp);
+5        return image;
+6    }
+7    public void answer(int[][] image, int r, int c, int color, int temp){
+8        if(r<0 || c<0 || r>=image.length || c>=image[0].length || image[r][c] != temp){
+9            return;
 10        }
-11
-12        for(int[] arr : edges){
-13            int u = arr[0];
-14            int v = arr[1];
+11        if(temp == color){
+12            return;
+13        }
+14        image[r][c] = color;
 15
-16            Queue<Integer> q = new LinkedList<>();
-17            HashSet<Integer> visited = new HashSet<>();
-18            q.add(u);
-19            while(!q.isEmpty()){
-20                int r = q.poll();
-21                if(visited.contains(r)){
-22                    continue;
-23                }
-24                visited.add(r);
-25                for(int nbrs : map.get(r)){
-26                    if(!visited.contains(nbrs)){
-27                        q.add(nbrs);
-28                    }
-29                }
-30            }
-31            if(visited.contains(v)){
-32                return arr;
-33            }
-34
-35
-36            map.get(u).add(v);
-37            map.get(v).add(u);
-38        }
-39        return new int[]{-1,-1};
-40
-41    }
-42}
+16        answer(image, r+1, c, color, temp);
+17        answer(image, r, c-1, color, temp);
+18        answer(image, r-1, c, color, temp);
+19        answer(image, r, c+1, color, temp);
+20
+21
+22
+23    }
+24}
