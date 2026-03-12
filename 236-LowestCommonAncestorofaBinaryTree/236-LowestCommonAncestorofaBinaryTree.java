604cf85dcc2e2ef13926b42c001f8b3cd56c32b2
Kuldeep Garg <kuldeepgarg6993@gmail.com>
2026-03-12 16:13:53 +0530

    File updated at 3/12/2026, 4:13:53 PM

--- 236-LowestCommonAncestorofaBinaryTree/236-LowestCommonAncestorofaBinaryTree.java
+++ 236-LowestCommonAncestorofaBinaryTree/236-LowestCommonAncestorofaBinaryTree.java
@@ -1,37 +1,55 @@
-// Last updated: 3/12/2026, 2:38:19 PM
+// Last updated: 3/12/2026, 4:13:53 PM
 1/**
 2 * Definition for a binary tree node.
 3 * public class TreeNode {
 4 *     int val;
 5 *     TreeNode left;
 6 *     TreeNode right;
-7 *     TreeNode(int x) { val = x; }
-8 * }
-9 */
-10class Solution {
-11    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-12        return answer(root, p, q);
-13    }
-14
-15    public static TreeNode answer(TreeNode root, TreeNode p, TreeNode q){
-16        if(root == null){
-17            return null;
-18        }
-19
-20        if(root == p || root == q){
-21            return root;
+7 *     TreeNode() {}
+8 *     TreeNode(int val) { this.val = val; }
+9 *     TreeNode(int val, TreeNode left, TreeNode right) {
+10 *         this.val = val;
+11 *         this.left = left;
+12 *         this.right = right;
+13 *     }
+14 * }
+15 */
+16class Solution {
+17    List<List<Integer>> ans ;
+18    public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
+19        ans = new ArrayList<>();
+20        if(root == null){
+21            return ans;
 22        }
-23
-24        TreeNode left = answer(root.left, p, q);
-25        TreeNode right = answer(root.right, p, q);
-26
-27        if(left != null && right != null){
-28            return root;
-29        }
-30        if(left == null){
-31            return right;
-32        }else{
-33            return left;
-34        }
-35    }
-36}
+23        answer(root);
+24        return ans;
+25    }
+26    public void answer(TreeNode root){
+27        Queue<TreeNode> q = new LinkedList<>();
+28        q.add(root);
+29        int level = 0;
+30        while(!q.isEmpty()){
+31            List<Integer> ll = new ArrayList<>();
+32            int size = q.size();
+33        
+34            for(int i=0 ;i<size ;i++){
+35                TreeNode r = q.poll();
+36                ll.add(r.val);
+37                if(r.left != null){
+38                    q.add(r.left);
+39                }
+40                if(r.right != null){
+41                    q.add(r.right);
+42                }
+43            }
+44
+45            if(level % 2 != 0){
+46                Collections.reverse(ll);
+47            }
+48            level++;
+49            ans.add(ll);
+50            
+51        }
+52
+53    }
+54}
