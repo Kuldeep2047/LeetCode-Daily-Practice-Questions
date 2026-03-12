67f360c7f15adb14be2d0319c00f1a9cd1c8c56c
Kuldeep Garg <kuldeepgarg6993@gmail.com>
2026-03-12 15:08:56 +0530

    File updated at 3/12/2026, 3:08:56 PM

--- 112-PathSum/112-PathSum.java
+++ 112-PathSum/112-PathSum.java
@@ -1,33 +1,39 @@
-// Last updated: 8/9/2025, 1:45:24 PM
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
-class Solution {
-    public boolean hasPathSum(TreeNode root, int targetSum) {
-        return isSum(root,targetSum);
-    }
-    public static boolean isSum(TreeNode root,int target){
-        if(root==null){
-            return false;
-        }
-        if(root.left == null && root.right==null){
-            return target-root.val==0;
-        }
-        boolean left = isSum(root.left,target-root.val);
-        boolean right = isSum(root.right,target-root.val);
-        return left || right;
-        
-    }
-}
+// Last updated: 3/12/2026, 3:08:56 PM
+1/**
+2 * Definition for a binary tree node.
+3 * public class TreeNode {
+4 *     int val;
+5 *     TreeNode left;
+6 *     TreeNode right;
+7 *     TreeNode() {}
+8 *     TreeNode(int val) { this.val = val; }
+9 *     TreeNode(int val, TreeNode left, TreeNode right) {
+10 *         this.val = val;
+11 *         this.left = left;
+12 *         this.right = right;
+13 *     }
+14 * }
+15 */
+16class Solution {
+17    public boolean hasPathSum(TreeNode root, int targetSum) {
+18        int sum =0;
+19        return answer(root, targetSum, sum);
+20    }
+21
+22    public boolean answer(TreeNode root, int tar, int sum){
+23        if(root == null){
+24            return false;
+25        }
+26        sum += root.val;
+27        if(root.left == null && root.right == null){
+28            if(tar == sum ){
+29                return true;
+30            }
+31        }
+32        
+33        boolean left = answer(root.left, tar, sum);
+34        boolean right = answer(root.right, tar, sum);
+35
+36        return left || right;
+37    }
+38}
