64ada0632c51dea1df0f0ed09ef31fdfebea4403
Kuldeep Garg <kuldeepgarg6993@gmail.com>
2026-03-12 14:52:15 +0530

    File updated at 3/12/2026, 2:52:15 PM

--- 543-DiameterofBinaryTree/543-DiameterofBinaryTree.java
+++ 543-DiameterofBinaryTree/543-DiameterofBinaryTree.java
@@ -1,4 +1,4 @@
-// Last updated: 3/12/2026, 2:39:37 PM
+// Last updated: 3/12/2026, 2:52:15 PM
 1/**
 2 * Definition for a binary tree node.
 3 * public class TreeNode {
@@ -15,29 +15,21 @@
 14 * }
 15 */
 16class Solution {
-17    public int diameterOfBinaryTree(TreeNode root) {
-18        return answer(root);
+17    public boolean isSymmetric(TreeNode root) {
+18        return answer(root.left, root.right);
 19    }
-20
-21    public int answer(TreeNode root){
-22        if(root == null){
-23            return 0;
-24        }
-25        int ld = answer(root.left);
-26        int rd = answer(root.right);
-27        int sd = height(root.left) + height(root.right) +2;
-28
-29        return Math.max(sd, Math.max(ld, rd));
-30    }
-31
-32    public int height(TreeNode root){
-33        if(root == null){
-34            return -1;
-35        }
-36
-37        int lh = height(root.left)+1;
-38        int rh = height(root.right) +1;
-39
-40        return Math.max(lh, rh);
-41    }
-42}
+20    public boolean answer(TreeNode root1, TreeNode root2){
+21        if(root1 == null && root2 == null){
+22            return true;
+23        }
+24        if(root1 == null || root2 == null){
+25            return false;
+26        }
+27        if(root1.val != root2.val){
+28            return false;
+29        }
+30        boolean left = answer(root1.left, root2.right);
+31        boolean right = answer(root1.right, root2.left);
+32        return left && right;
+33    }
+34}
