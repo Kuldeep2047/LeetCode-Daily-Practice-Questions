27cdf4265d31cea7888b9c07d753888e971fa16d
Kuldeep Garg <kuldeepgarg6993@gmail.com>
2026-03-12 14:38:19 +0530

    File updated at 3/12/2026, 2:38:19 PM

--- 236-LowestCommonAncestorofaBinaryTree/236-LowestCommonAncestorofaBinaryTree.java
+++ 236-LowestCommonAncestorofaBinaryTree/236-LowestCommonAncestorofaBinaryTree.java
@@ -1,4 +1,4 @@
-// Last updated: 3/12/2026, 11:32:27 AM
+// Last updated: 3/12/2026, 2:38:19 PM
 1/**
 2 * Definition for a binary tree node.
 3 * public class TreeNode {
@@ -10,20 +10,20 @@
 9 */
 10class Solution {
 11    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-12        return LCA(root,p,q);
+12        return answer(root, p, q);
 13    }
 14
-15    public TreeNode LCA(TreeNode root, TreeNode p, TreeNode q){
+15    public static TreeNode answer(TreeNode root, TreeNode p, TreeNode q){
 16        if(root == null){
 17            return null;
 18        }
-19        if(root == p || q == root){
-20            return root;
-21        }
-22
+19
+20        if(root == p || root == q){
+21            return root;
+22        }
 23
-24        TreeNode left = LCA(root.left,p,q);
-25        TreeNode right = LCA(root.right,p,q);
+24        TreeNode left = answer(root.left, p, q);
+25        TreeNode right = answer(root.right, p, q);
 26
 27        if(left != null && right != null){
 28            return root;
