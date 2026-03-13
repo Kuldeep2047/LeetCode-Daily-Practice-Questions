29a886fb0f7ada10981d4126d17af53f73f084f6
Kuldeep Garg <kuldeepgarg6993@gmail.com>
2026-03-13 11:17:37 +0530

    File updated at 3/13/2026, 11:17:37 AM

--- 230-KthSmallestElementinaBST/230-KthSmallestElementinaBST.java
+++ 230-KthSmallestElementinaBST/230-KthSmallestElementinaBST.java
@@ -1,42 +1,40 @@
-// Last updated: 8/15/2025, 4:50:43 PM
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
-class Solution {
-    boolean isfound = false;
-    int max;
-    int c= 0;
-    public int kthSmallest(TreeNode root, int k) {
-        smallest(root,k);
-        return max;
-    }
-    public void smallest(TreeNode root,int k){
-        if(root == null){
-            return;
-        }
-        if(isfound){
-            return;
-        }
-        smallest(root.left,k);
-        
-        c++;
-        if(c==k){
-            max = root.val;
-            isfound = true;
-        }
-        
-        smallest(root.right,k);
-    }
-}
+// Last updated: 3/13/2026, 11:17:37 AM
+1/**
+2 * Definition for a binary tree node.
+3 * public class TreeNode {
+4 *     int val;
+5 *     TreeNode left;
+6 *     TreeNode right;
+7 *     TreeNode() {}
+8 *     TreeNode(int val) { this.val = val; }
+9 *     TreeNode(int val, TreeNode left, TreeNode right) {
+10 *         this.val = val;
+11 *         this.left = left;
+12 *         this.right = right;
+13 *     }
+14 * }
+15 */
+16class Solution {
+17    int min = 0;
+18    boolean tillK = false;
+19    int c= 0;
+20    public int kthSmallest(TreeNode root, int k) {
+21        answer(root, k);
+22        return min;
+23    }
+24    public void answer(TreeNode root, int k){
+25        if(root == null){
+26            return;
+27        }
+28        if(tillK == true){
+29            return ;
+30        }
+31        answer(root.left, k);
+32        c++;
+33        if(c == k){
+34            min = root.val;
+35            tillK = true;
+36        }
+37        answer(root.right, k);
+38    }
+39}
