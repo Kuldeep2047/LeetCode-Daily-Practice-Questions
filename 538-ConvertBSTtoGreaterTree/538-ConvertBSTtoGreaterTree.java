bf049d5111b66560fa739408c2f2e7df816405ca
Kuldeep Garg <kuldeepgarg6993@gmail.com>
2026-03-13 14:27:33 +0530

    File updated at 3/13/2026, 2:27:32 PM

--- 538-ConvertBSTtoGreaterTree/538-ConvertBSTtoGreaterTree.java
+++ 538-ConvertBSTtoGreaterTree/538-ConvertBSTtoGreaterTree.java
@@ -1,32 +1,32 @@
-// Last updated: 8/23/2025, 10:36:33 PM
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
-class Solution {
-    public TreeNode convertBST(TreeNode root) {
-        return answer(root);
-    }
-    int sum =0;
-    public TreeNode answer(TreeNode root){
-        if(root == null){
-            return null;
-        }
-        answer(root.right);
-        sum += root.val;
-        root.val = sum;
-        answer(root.left);
-        return root;
-    }
-}
+// Last updated: 3/13/2026, 2:27:32 PM
+1/**
+2 * Definition for a binary tree node.
+3 * public class TreeNode {
+4 *     int val;
+5 *     TreeNode left;
+6 *     TreeNode right;
+7 *     TreeNode() {}
+8 *     TreeNode(int val) { this.val = val; }
+9 *     TreeNode(int val, TreeNode left, TreeNode right) {
+10 *         this.val = val;
+11 *         this.left = left;
+12 *         this.right = right;
+13 *     }
+14 * }
+15 */
+16class Solution {
+17    public TreeNode convertBST(TreeNode root) {
+18        return answer(root);
+19    }
+20    int sum =0;
+21    public TreeNode answer(TreeNode root){
+22        if(root == null){
+23            return null;
+24        }
+25        answer(root.right);
+26        sum += root.val;
+27        root.val = sum;
+28        answer(root.left);
+29        return root;
+30    }
+31}
