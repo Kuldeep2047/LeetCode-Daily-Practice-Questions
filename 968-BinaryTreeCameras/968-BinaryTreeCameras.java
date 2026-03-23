3f1b14c51877533191ef2fd4ef84d3041d825b0e
Kuldeep Garg <kuldeepgarg6993@gmail.com>
2026-03-23 15:47:02 +0530

    File updated at 3/23/2026, 3:47:01 PM

--- 968-BinaryTreeCameras/968-BinaryTreeCameras.java
+++ 968-BinaryTreeCameras/968-BinaryTreeCameras.java
@@ -1,4 +1,4 @@
-// Last updated: 3/23/2026, 2:30:41 PM
+// Last updated: 3/23/2026, 3:47:01 PM
 1/**
 2 * Definition for a binary tree node.
 3 * public class TreeNode {
@@ -15,30 +15,29 @@
 14 * }
 15 */
 16class Solution {
-17    int Camera=0;
-18    public int minCameraCover(TreeNode root) {
-19        int c= minCamera(root);
-20        if(c==-1) {
-21            Camera++;
-22        }
-23        return Camera;
-24        
-25    }
-26    public int minCamera(TreeNode root) {
-27        if(root == null) {
-28            return 0;
-29        }
-30        int left = minCamera(root.left);
-31        int right = minCamera(root.right);
-32        if(left == -1 || right ==-1) {	//is node per camera ki need hai
-33            Camera++;
-34            return 1;	//camera setup kiya hai is node per
+17    public int diameterOfBinaryTree(TreeNode root) {
+18        return answer(root);
+19    }
+20
+21    public int answer(TreeNode root){
+22        if(root == null){
+23            return 0;
+24        }
+25        int ld = answer(root.left);
+26        int rd = answer(root.right);
+27        int sd = height(root.left) + height(root.right) +2;
+28
+29        return Math.max(sd, Math.max(ld, rd));
+30    }
+31
+32    public int height(TreeNode root){
+33        if(root == null){
+34            return -1;
 35        }
-36        else if(left == 1 || right == 1) {	//inme se koi ek ke pass ya dono ke pass camera hai or ek ke pass camera hai and dusra wala covered hai
-37            return 0;	//iska mtlb me covered hu
-38        }else {
-39            return -1;	//need a Camera
-40        }
-41        
-42    }
-43}
+36
+37        int lh = height(root.left)+1;
+38        int rh = height(root.right) +1;
+39
+40        return Math.max(lh, rh);
+41    }
+42}
