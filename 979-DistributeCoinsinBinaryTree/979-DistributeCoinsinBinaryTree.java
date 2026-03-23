db56cf15a875d84fc0f339803d40b61b0d4b0c79
Kuldeep Garg <kuldeepgarg6993@gmail.com>
2026-03-23 15:14:04 +0530

    File updated at 3/23/2026, 3:14:03 PM

--- 979-DistributeCoinsinBinaryTree/979-DistributeCoinsinBinaryTree.java
+++ 979-DistributeCoinsinBinaryTree/979-DistributeCoinsinBinaryTree.java
@@ -1,35 +1,35 @@
-// Last updated: 8/13/2025, 12:13:12 PM
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
-class Solution {
-    int ans =0;
-    public int distributeCoins(TreeNode root) {
-        minmove(root);
-        return ans;
-        
-    }
-    public int minmove(TreeNode root) {
-        if(root == null) {
-            return 0;
-        }
-        
-        int left = minmove(root.left);
-        int right = minmove(root.right);
-        ans = ans + Math.abs(left)+ Math.abs(right);
-        return root.val+ left + right -1;
-        
-    }
-}
+// Last updated: 3/23/2026, 3:14:03 PM
+1/**
+2 * Definition for a binary tree node.
+3 * public class TreeNode {
+4 *     int val;
+5 *     TreeNode left;
+6 *     TreeNode right;
+7 *     TreeNode() {}
+8 *     TreeNode(int val) { this.val = val; }
+9 *     TreeNode(int val, TreeNode left, TreeNode right) {
+10 *         this.val = val;
+11 *         this.left = left;
+12 *         this.right = right;
+13 *     }
+14 * }
+15 */
+16class Solution {
+17    int ans =0;
+18    public int distributeCoins(TreeNode root) {
+19        minmove(root);
+20        return ans;
+21        
+22    }
+23    public int minmove(TreeNode root) {
+24        if(root == null){ 
+25            return 0;
+26        }
+27        
+28        int left = minmove(root.left);
+29        int right = minmove(root.right);
+30        ans = ans + Math.abs(left)+ Math.abs(right);
+31        return root.val+ left + right -1;
+32        
+33    }
+34}
