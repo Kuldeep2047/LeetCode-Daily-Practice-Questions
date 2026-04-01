21b7f398a94c7a9dd6ef4d041acdfcc0841321ba
Kuldeep Garg <kuldeepgarg6993@gmail.com>
2026-04-01 15:21:36 +0530

    File updated at 4/1/2026, 3:21:35 PM

--- 136-SingleNumber/136-SingleNumber.java
+++ 136-SingleNumber/136-SingleNumber.java
@@ -1,13 +1,6 @@
-// Last updated: 4/1/2026, 3:19:07 PM
+// Last updated: 4/1/2026, 3:21:35 PM
 1class Solution {
-2    public int singleNumber(int[] nums) {
-3        return answer(nums);
+2    public boolean isPowerOfTwo(int n) {
+3        return n > 0 && (n & (n - 1)) == 0;
 4    }
-5    public int answer(int[] arr){
-6        int xor = 0;
-7        for(int a : arr){
-8            xor ^= a;
-9        }
-10        return xor;
-11    }
-12}
+5}
