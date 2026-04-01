9fd742b983ecc8908ca04c1b85670c67ff59979c
Kuldeep Garg <kuldeepgarg6993@gmail.com>
2026-04-01 15:19:07 +0530

    File updated at 4/1/2026, 3:19:07 PM

--- 136-SingleNumber/136-SingleNumber.java
+++ 136-SingleNumber/136-SingleNumber.java
@@ -1,10 +1,13 @@
-// Last updated: 11/27/2025, 12:20:12 PM
+// Last updated: 4/1/2026, 3:19:07 PM
 1class Solution {
 2    public int singleNumber(int[] nums) {
-3        int n=0;
-4        for(int i:nums){
-5            n^=i;
-6        }
-7        return n;
-8    }
-9}
+3        return answer(nums);
+4    }
+5    public int answer(int[] arr){
+6        int xor = 0;
+7        for(int a : arr){
+8            xor ^= a;
+9        }
+10        return xor;
+11    }
+12}
