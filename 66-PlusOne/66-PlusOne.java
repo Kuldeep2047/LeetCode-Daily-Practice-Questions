261a952d2cd01aaa4e4bf5201d8224a65010909e
Kuldeep Garg <kuldeepgarg6993@gmail.com>
2026-04-06 14:32:26 +0530

    File updated at 4/6/2026, 2:32:25 PM

--- 66-PlusOne/66-PlusOne.java
+++ 66-PlusOne/66-PlusOne.java
@@ -1,20 +1,22 @@
-// Last updated: 8/9/2025, 1:45:50 PM
-class Solution {
-    public int[] plusOne(int[] digits) {
-        return Plus_One(digits);
-    }
-    public int[] Plus_One(int[] digits){
-        int n = digits.length;
-        int[] nums = new int[n+1];
-        for(int i=n-1;i>=0;i--){
-            if(digits[i]<9){
-                digits[i]++;
-                return digits;
-            }
-            digits[i] = 0;
-        }
-
-        nums[0] =1;
-        return nums;
-    }
-}
+// Last updated: 4/6/2026, 2:32:25 PM
+1class Solution {
+2    public int[] plusOne(int[] digits) {
+3        return Plus_One(digits);
+4    }
+5
+6    
+7    public int[] Plus_One(int[] digits){
+8        int n = digits.length;
+9        int[] nums = new int[n+1];
+10        for(int i=n-1;i>=0;i--){
+11            if(digits[i]<9){
+12                digits[i]++;
+13                return digits;
+14            }
+15            digits[i] = 0;
+16        }
+17
+18        nums[0] =1;
+19        return nums;
+20    }
+21}
