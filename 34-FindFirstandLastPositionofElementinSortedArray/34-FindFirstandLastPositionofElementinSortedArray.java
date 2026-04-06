9620d32ea755a0d182891d349cec8ffc425a5828
Kuldeep Garg <kuldeepgarg6993@gmail.com>
2026-04-06 14:52:38 +0530

    File updated at 4/6/2026, 2:52:38 PM

--- 34-FindFirstandLastPositionofElementinSortedArray/34-FindFirstandLastPositionofElementinSortedArray.java
+++ 34-FindFirstandLastPositionofElementinSortedArray/34-FindFirstandLastPositionofElementinSortedArray.java
@@ -1,26 +1,34 @@
-// Last updated: 12/18/2025, 10:25:49 PM
+// Last updated: 4/6/2026, 2:52:38 PM
 1class Solution {
 2    public int[] searchRange(int[] nums, int target) {
-3        return answer(nums, target);
-4    }
-5
-6    public int[] answer(int[] arr ,int tar){
-7        int n = arr.length;
-8        int[] ans = {-1,-1};
-9
-10        for(int i=0 ;i<n ;i++){
-11            if(arr[i] == tar){
-12                ans[0] = i;
-13                break;
-14            }
-15        }
-16
-17        for(int i=n-1 ;i>=0 ;i--){
-18            if(arr[i] == tar){
-19                ans[1] = i;
-20                break;
-21            }
-22        }
-23        return ans;
-24    }
-25}
+3        int[] ans = new int[2];
+4        ans[0] = answer(nums, target, true);
+5        ans[1] = answer(nums, target, false);
+6        return ans;
+7    }
+8
+9    public int answer(int[] nums ,int target, boolean isLeft){
+10        int left = 0;
+11        int right = nums.length - 1;
+12        int idx = -1;
+13
+14        while (left <= right) {
+15            int mid = left + (right - left) / 2;
+16            
+17            if (nums[mid] > target) {
+18                right = mid - 1;
+19            } else if (nums[mid] < target) {
+20                left = mid + 1;
+21            } else {
+22                idx = mid;
+23                if (isLeft) {
+24                    right = mid - 1;
+25                } else {
+26                    left = mid + 1;
+27                }
+28            }
+29        }
+30
+31        return idx;
+32    }
+33}
