3fed409ad1ad9c5a63768e11e0ee7327df37af2b
Kuldeep Garg <kuldeepgarg6993@gmail.com>
2026-04-06 15:15:47 +0530

    File updated at 4/6/2026, 3:15:46 PM

--- 54-SpiralMatrix/54-SpiralMatrix.java
+++ 54-SpiralMatrix/54-SpiralMatrix.java
@@ -1,38 +1,48 @@
-// Last updated: 4/6/2026, 3:14:52 PM
+// Last updated: 4/6/2026, 3:15:46 PM
 1class Solution {
 2    public List<Integer> spiralOrder(int[][] matrix) {
-3        return Spiral_Print(matrix);
+3        return answer(matrix);
 4    }
-5    public List<Integer> Spiral_Print(int[][] arr){
-6        int minr =0;
-7        int maxr = arr.length-1;
-8        int minc =0;
-9        int maxc = arr[0].length-1;
-10        int te = arr.length * arr[0].length;
+5
+6    public List<Integer> answer(int[][] arr){
+7        int n = arr.length;
+8        int m = arr[0].length;
+9        List<Integer> ll = new ArrayList<>();
+10        int te = n * m;
 11        int c =0;
-12        List<Integer> ll = new ArrayList<>();
-13        while(c<te){
-14            for(int i= minc ; i<=maxc && c<te ;i++){
-15                ll.add(arr[minr][i]);
-16                c++;
-17            }
-18            minr++;
-19            for(int i = minr;i<=maxr && c<te ;i++){
-20                ll.add(arr[i][maxc]);
-21                c++;
-22            }
-23            maxc--;
-24            for(int i = maxc ;i>=minc && c<te ;i--){
-25                ll.add(arr[maxr][i]);
+12
+13        int minr = 0;
+14        int maxr = n-1;
+15        int minc = 0;
+16        int maxc = m-1;
+17        while(c < te){
+18            for(int i = minc ; i<=maxc && c < te ; i++){
+19                ll.add(arr[minr][i]);
+20                c++;
+21            }
+22            minr++;
+23
+24            for(int i = minr ;i<= maxr && c < te;i++){
+25                ll.add(arr[i][maxc]);
 26                c++;
 27            }
-28            maxr--;
-29            for(int i = maxr ; i>=minr && c<te ;i--){
-30                ll.add(arr[i][minc]);
-31                c++;
-32            }
-33            minc++;
-34        }
-35        return ll;
-36    }
-37}
+28            maxc--;
+29
+30            for(int i = maxc ; i>= minc && c < te ;i--){
+31                ll.add(arr[maxr][i]);
+32                c++;
+33            }
+34            maxr--;
+35
+36            for(int i = maxr ; i >= minr && c < te ;i--){
+37                ll.add(arr[i][minc]);
+38                c++;
+39            }
+40            minc++;
+41        }
+42        return ll;
+43
+44    }
+45
+46
+47}
