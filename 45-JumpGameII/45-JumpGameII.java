8e8b5c9fdee1a4dbb980fc725467aa0183de3d7b
Kuldeep Garg <kuldeepgarg6993@gmail.com>
2026-05-14 22:33:02 +0530

    File updated at 5/14/2026, 10:33:02 PM

--- 45-JumpGameII/45-JumpGameII.java
+++ 45-JumpGameII/45-JumpGameII.java
@@ -1,20 +1,24 @@
-// Last updated: 5/14/2026, 10:00:35 PM
+// Last updated: 5/14/2026, 10:33:02 PM
 1class Solution {
-2    public int jump(int[] nums) {
-3        return Jump_2(nums);
-4    }
-5    public static int Jump_2(int[] arr){
-6        int n = arr.length;
-7        int farthest = 0;
-8        int jump =0;
-9        int end = 0;
-10        for(int i = 0; i< n-1;i++){
-11            farthest = Math.max(farthest, arr[i]+i);
-12            if(i == end){
-13                jump++;
-14                end = farthest;
-15            }
-16        }
-17        return jump;
-18    }
-19}
+2    public boolean canReach(int[] arr, int start) {
+3        boolean[] visited = new boolean[arr.length];
+4        return answer(arr, start, visited);
+5    }
+6    public boolean answer(int[] arr, int idx, boolean[] visited){
+7        if(idx < 0 || idx >= arr.length){
+8            return false;
+9        }
+10        if(visited[idx] == true){
+11            return false;
+12        }
+13        if(arr[idx] == 0){
+14            return true;
+15        }
+16        
+17        visited[idx] = true;
+18        boolean a = answer(arr, idx + arr[idx], visited);
+19        boolean b = answer(arr, idx - arr[idx], visited);
+20
+21        return a || b;
+22    }
+23}
