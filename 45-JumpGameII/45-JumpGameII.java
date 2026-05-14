92a23005c92463e29e5a77442e624993cbbdb67f
Kuldeep Garg <kuldeepgarg6993@gmail.com>
2026-05-14 22:00:35 +0530

    File updated at 5/14/2026, 10:00:35 PM

--- 45-JumpGameII/45-JumpGameII.java
+++ 45-JumpGameII/45-JumpGameII.java
@@ -1,20 +1,20 @@
-// Last updated: 10/2/2025, 8:59:32 PM
-class Solution {
-    public int jump(int[] nums) {
-        return Jump_2(nums);
-    }
-    public static int Jump_2(int[] arr){
-        int n = arr.length;
-        int farthest = 0;
-        int jump =0;
-        int end = 0;
-        for(int i = 0; i< n-1;i++){
-            farthest = Math.max(farthest, arr[i]+i);
-            if(i == end){
-                jump++;
-                end = farthest;
-            }
-        }
-        return jump;
-    }
-}
+// Last updated: 5/14/2026, 10:00:35 PM
+1class Solution {
+2    public int jump(int[] nums) {
+3        return Jump_2(nums);
+4    }
+5    public static int Jump_2(int[] arr){
+6        int n = arr.length;
+7        int farthest = 0;
+8        int jump =0;
+9        int end = 0;
+10        for(int i = 0; i< n-1;i++){
+11            farthest = Math.max(farthest, arr[i]+i);
+12            if(i == end){
+13                jump++;
+14                end = farthest;
+15            }
+16        }
+17        return jump;
+18    }
+19}
