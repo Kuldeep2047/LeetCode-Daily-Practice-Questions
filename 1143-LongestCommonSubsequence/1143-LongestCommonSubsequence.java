28273e58f869b4dd17a5fe0e788c67aff13f64c1
Kuldeep Garg <kuldeepgarg6993@gmail.com>
2026-07-27 11:26:00 +0530

    File updated at 7/27/2026, 11:26:00 AM

--- 1143-LongestCommonSubsequence/1143-LongestCommonSubsequence.java
+++ 1143-LongestCommonSubsequence/1143-LongestCommonSubsequence.java
@@ -1,4 +1,4 @@
-// Last updated: 5/6/2026, 10:33:29 PM
+// Last updated: 7/27/2026, 11:26:00 AM
 1class Solution {
 2    int[][] dp;
 3    public int longestCommonSubsequence(String text1, String text2) {
@@ -11,22 +11,21 @@
 10    }
 11
 12    public int answer(String s1, String s2, int i, int j){
-13        if(i == s1.length() || j == s2.length() ){
+13        if(i == s1.length() || j == s2.length()){
 14            return 0;
 15        }
-16        if(dp[i][j] != -1){
-17            return dp[i][j];
-18        }
-19
+16
+17        if(dp[i][j] != -1){
+18            return dp[i][j];
+19        }
 20        int ans =0;
 21        if(s1.charAt(i) == s2.charAt(j)){
-22            ans = 1 + answer(s1, s2, i+1, j+1);
+22            ans = 1 + answer(s1,s2,i+1, j+1);
 23        }
 24        else{
-25            int a = answer(s1, s2, i+1, j);
-26            int b = answer(s1, s2, i, j+1);
-27            ans = Math.max(a,b);
-28        }
-29        return dp[i][j] =  ans;
-30    }
-31}
+25            ans = Math.max(answer(s1,s2,i+1, j) , answer(s1,s2, i, j+1));
+26        }
+27 
+28        return dp[i][j] = ans;
+29    }
+30}
