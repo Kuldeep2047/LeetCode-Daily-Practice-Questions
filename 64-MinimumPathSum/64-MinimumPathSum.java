893546ad5538f29ad7b8584fa1221c75d6500296
Kuldeep Garg <kuldeepgarg6993@gmail.com>
2026-07-27 10:48:40 +0530

    File updated at 7/27/2026, 10:48:40 AM

--- 64-MinimumPathSum/64-MinimumPathSum.java
+++ 64-MinimumPathSum/64-MinimumPathSum.java
@@ -1,28 +1,38 @@
-// Last updated: 1/3/2026, 3:07:50 PM
+// Last updated: 7/27/2026, 10:48:40 AM
 1class Solution {
-2    public int minPathSum(int[][] grid) {
-3        int[][] dp = new int[grid.length][grid[0].length];
-4        for(int[] a : dp){
-5            Arrays.fill(a, 999999);
-6        }
-7        return answer(grid, 0, 0, dp);
-8    }
-9    public int answer(int[][] arr, int cr, int cc, int[][] dp){
-10        if(cr == arr.length-1 && cc == arr[0].length -1){
-11            return arr[cr][cc];
+2    public int minFallingPathSum(int[][] matrix) {
+3        return answer(matrix);
+4    }
+5
+6    public int answer(int[][] arr){
+7        int n = arr.length;
+8        int m = arr[0].length;
+9        int[][] ans = new int[n][m];
+10        for(int i=0 ;i<m ;i++){
+11            ans[0][i] = arr[0][i];
 12        }
-13        if(cr >= arr.length || cc>=arr[0].length){
-14            return Integer.MAX_VALUE;
-15        }
-16
-17        if(dp[cr][cc] != 999999){
-18            return dp[cr][cc];
-19        }
-20
-21        int right = answer(arr, cr, cc+1, dp);
-22        int down = answer(arr, cr+1, cc, dp);
-23
-24        return dp[cr][cc] = Math.min(right, down) + arr[cr][cc];
-25    }
+13
+14        for(int i=1 ;i<n ;i++){
+15            for(int j= 0 ;j<m ;j++){
+16                int top = ans[i-1][j];
+17                
+18                int ld = Integer.MAX_VALUE;
+19                if(i>0 && j>0){
+20                    ld = ans[i-1][j-1];
+21                }
+22                int rd = Integer.MAX_VALUE;
+23                if(j<m-1){
+24                    rd = ans[i-1][j+1];
+25                }
 26
-27}
+27                ans[i][j] = arr[i][j] + Math.min(top, Math.min(ld, rd));
+28            }
+29        }
+30
+31        int res = Integer.MAX_VALUE;
+32        for(int i=0 ; i<m ;i++){
+33            res = Math.min(res, ans[n-1][i]);
+34        }
+35        return res;
+36    }
+37}
