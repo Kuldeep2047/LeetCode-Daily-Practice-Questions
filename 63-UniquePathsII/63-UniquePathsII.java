e4eda2e7e9e8b3255d6cd87a317a1d9c6460a5f0
Kuldeep Garg <kuldeepgarg6993@gmail.com>
2026-07-27 16:28:18 +0530

    File updated at 7/27/2026, 4:28:18 PM

--- 63-UniquePathsII/63-UniquePathsII.java
+++ 63-UniquePathsII/63-UniquePathsII.java
@@ -1,4 +1,4 @@
-// Last updated: 5/16/2026, 9:46:34 PM
+// Last updated: 7/27/2026, 4:28:18 PM
 1class Solution {
 2    public int uniquePathsWithObstacles(int[][] obstacleGrid) {
 3        int m = obstacleGrid.length;
@@ -15,17 +15,14 @@
 14        if(cr == er && cc == ec){
 15            return 1;
 16        }
-17
-18        
-19
-20        if(dp[cr][cc] != 0){
-21            return dp[cr][cc];
-22        }
-23        
-24        int a = paths(cr +1, cc, er, ec, dp, obstacle);
-25        int b = paths(cr , cc+1, er, ec, dp, obstacle);
-26
-27        return dp[cr][cc] = a+b;
-28        
-29    }
-30}
+17        if(dp[cr][cc] != 0){
+18            return dp[cr][cc];
+19        }
+20        
+21        int a = paths(cr +1, cc, er, ec, dp, obstacle);
+22        int b = paths(cr , cc+1, er, ec, dp, obstacle);
+23
+24        return dp[cr][cc] = a+b;
+25        
+26    }
+27}
