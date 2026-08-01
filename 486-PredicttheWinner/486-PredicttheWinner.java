52f1efa9cc397f2be9489ecdf6cd5c549e2c2850
Kuldeep Garg <kuldeepgarg6993@gmail.com>
2026-08-01 15:10:41 +0530

    File updated at 8/1/2026, 3:10:40 PM

--- 486-PredicttheWinner/486-PredicttheWinner.java
+++ 486-PredicttheWinner/486-PredicttheWinner.java
@@ -1,26 +1,26 @@
-// Last updated: 10/13/2025, 7:27:05 PM
-class Solution {
-    public boolean predictTheWinner(int[] nums) {
-        int[][] dp = new int[nums.length][nums.length];
-
-        for(int[] r : dp){
-            Arrays.fill(r, -1);
-        }
-        int score_diff = Score(nums, 0, nums.length-1,dp);
-        return score_diff >=0;
-    }
-
-    public int Score(int[] nums, int l, int r, int[][] dp){
-        if(l == r){
-            return nums[l];
-        }
-        if(dp[l][r] != -1){
-            return dp[l][r];
-        }
-
-        int left = nums[l] - Score(nums, l+1, r, dp);
-        int right = nums[r] - Score(nums, l, r-1, dp);
-
-        return dp[l][r] = Math.max(left, right);
-    }
-}
+// Last updated: 8/1/2026, 3:10:40 PM
+1class Solution {
+2    public boolean predictTheWinner(int[] nums) {
+3        int[][] dp = new int[nums.length][nums.length];
+4
+5        for(int[] r : dp){
+6            Arrays.fill(r, -1);
+7        }
+8        int score_diff = Score(nums, 0, nums.length-1,dp);
+9        return score_diff >=0;
+10    }
+11
+12    public int Score(int[] nums, int l, int r, int[][] dp){
+13        if(l == r){
+14            return nums[l];
+15        }
+16        if(dp[l][r] != -1){
+17            return dp[l][r];
+18        }
+19
+20        int left = nums[l] - Score(nums, l+1, r, dp);
+21        int right = nums[r] - Score(nums, l, r-1, dp);
+22
+23        return dp[l][r] = Math.max(left, right);
+24    }
+25}
