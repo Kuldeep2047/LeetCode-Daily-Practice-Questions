534b8894694b1c123f4a447f64c1a5b472dcb477
Kuldeep Garg <kuldeepgarg6993@gmail.com>
2026-08-04 14:02:13 +0530

    File updated at 8/4/2026, 2:02:13 PM

--- 74-Searcha2DMatrix/74-Searcha2DMatrix.java
+++ 74-Searcha2DMatrix/74-Searcha2DMatrix.java
@@ -1,17 +1,17 @@
-// Last updated: 8/4/2026, 1:46:01 PM
+// Last updated: 8/4/2026, 2:02:13 PM
 1class Solution {
 2    public boolean searchMatrix(int[][] matrix, int target) {
-3        return answer(matrix, target);
+3        return answer(matrix , target);
 4    }
-5
-6    public boolean answer(int[][] arr ,int target){
-7        
-8        int r =0;
-9        int c = arr[0].length-1;
-10        while(r< arr.length && c>=0){
-11            if(arr[r][c] == target){
-12                return true;
-13            }else if(arr[r][c]< target){
+5    public boolean answer(int[][] arr ,int target){
+6        int r = 0;
+7        int c = arr[0].length-1;
+8
+9        while(r< arr.length && c>=0){
+10            if(arr[r][c] == target){
+11                return true;
+12            }
+13            if(arr[r][c] < target){
 14                r++;
 15            }else{
 16                c--;
