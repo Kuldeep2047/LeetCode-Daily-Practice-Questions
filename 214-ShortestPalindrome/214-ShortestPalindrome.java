b774b85e20ac375026ebc093ed5e05eeaf3efbb4
Kuldeep Garg <kuldeepgarg6993@gmail.com>
2026-08-05 13:14:16 +0530

    File updated at 8/5/2026, 1:14:14 PM

--- 214-ShortestPalindrome/214-ShortestPalindrome.java
+++ 214-ShortestPalindrome/214-ShortestPalindrome.java
@@ -1,26 +1,25 @@
-// Last updated: 8/9/2025, 1:44:29 PM
-class Solution {
-    public String shortestPalindrome(String s) {
-        return answer(s);
-    }
-    public static String answer(String s){
-        int n = s.length();
-        int i=0;
-        for(int j=n-1;j>=0;j--){
-            if(s.charAt(i) == s.charAt(j)){
-                i++;
-            }
-        }
-        if(i==n){
-            return s;
-        }
-
-        // StringBuilder last = new StringBuilder(s.substring(i,n)).reverse().toString();
-        StringBuilder start = new StringBuilder(s.substring(i,n));
-        start.reverse().toString();
-
-        // String mid = s.substring(0,i);
-
-        return start + answer(s.substring(0,i))+s.substring(i);
-    }
-}
+// Last updated: 8/5/2026, 1:14:14 PM
+1class Solution {
+2    public String shortestPalindrome(String s) {
+3        return answer(s);
+4    }
+5    public static String answer(String s){
+6        int n = s.length();
+7        int i=0;
+8        for(int j=n-1;j>=0;j--){
+9            if(s.charAt(i) == s.charAt(j)){
+10                i++;
+11            }
+12        }
+13        if(i==n){
+14            return s;
+15        }
+16
+17       
+18        StringBuilder start = new StringBuilder(s.substring(i,n));
+19        start.reverse().toString();
+20
+21
+22        return start + answer(s.substring(0,i))+s.substring(i);
+23    }
+24}
