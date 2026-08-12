aeb181a111dca3e4e6e5a4a80d0419520f7b4f3e
Kuldeep Garg <kuldeepgarg6993@gmail.com>
2026-08-12 09:18:53 +0530

    File updated at 8/12/2026, 9:18:52 AM

--- 239-SlidingWindowMaximum/239-SlidingWindowMaximum.java
+++ 239-SlidingWindowMaximum/239-SlidingWindowMaximum.java
@@ -1,60 +1,38 @@
-// Last updated: 9/28/2025, 2:33:26 PM
-class Solution {
-    public int[] maxSlidingWindow(int[] nums, int k) {
-        // return answer(nums,k);
-        return window_Maximum(nums, k);
-    }
-
-    public static int[] window_Maximum(int[] arr ,int k){
-        Deque<Integer> dq = new ArrayDeque<>();
-        int n = arr.length;
-        int[] ans = new int[n-k+1];
-
-        for(int i=0;i<k;i++){
-            while( ! dq.isEmpty() && arr[i]> arr[dq.peekLast()]){
-                dq.pollLast();
-            }
-            dq.add(i);
-        }
-        int idx =0;
-        for(int i=k ;i<n;i++){
-            ans[idx++] = arr[dq.peekFirst()];
-
-            //remove element from deque which is not part of current window
-            while( !dq.isEmpty() && dq.peekFirst() < i-k+1){
-                dq.pollFirst();
-            }
-
-            //remove element index jo deque me element se bade ho
-            while( ! dq.isEmpty() && arr[i]> arr[dq.peekLast()]){
-                dq.pollLast();
-            }
-            dq.add(i);
-        }
-        ans[idx++] = arr[dq.peekFirst()];
-
-        return ans;
-    }
-
-
-
-    // public static int[] answer(int[] nums,int k){
-    //     int n = nums.length;
-    //     int[] ans= new int[n - k + 1];
-    //     Deque<Integer> dq = new ArrayDeque<>();
-
-    //     for (int i = 0; i < n; i++) {
-    //         while (!dq.isEmpty() && dq.peekFirst() <= i - k) {
-    //             dq.pollFirst();
-    //         }
-    //         while (!dq.isEmpty() && nums[dq.peekLast()] < nums[i]) {
-    //             dq.pollLast();
-    //         }
-    //         dq.offerLast(i);
-    //         if (i >= k - 1) {
-    //             ans[i - k + 1] = nums[dq.peekFirst()];
-    //         }
-    //     }
-    //     return ans;
-    // }
-}
+// Last updated: 8/12/2026, 9:18:52 AM
+1class Solution {
+2    public int[] maxSlidingWindow(int[] nums, int k) {
+3        return answer(nums, k);
+4    }
+5
+6    public int[] answer(int[] arr ,int k){
+7        int n = arr.length;
+8
+9        int[] prefix = new int[n];
+10        for(int i=0 ;i<n ;i++){
+11            if(i % k== 0){
+12                prefix[i] = arr[i];
+13            }
+14            else{
+15                prefix[i] = Math.max(prefix[i-1], arr[i]);
+16            }
+17        }
+18
+19        int[] suffix = new int[n];
+20        for(int i = n-1 ; i>=0 ;i--){
+21            if(i%k == 0 || i == n-1){
+22                suffix[i] = arr[i];
+23            }else{
+24                suffix[i] = Math.max(suffix[i+1], arr[i]);
+25            }
+26        }
+27
+28        int[] ans = new int[n-k+1];
+29
+30        for(int i= k-1 ;i<n ;i++){
+31            int j = i-k+1;
+32            ans[j] = Math.max(prefix[i], suffix[j]);
+33        }
+34        return ans;
+35
+36    }
+37}
