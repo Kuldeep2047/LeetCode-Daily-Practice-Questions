ffa7e2f5767c9ac65ed868b9bb07cd9dee419b9d
Kuldeep Garg <kuldeepgarg6993@gmail.com>
2026-09-20 17:02:09 +0530

    File updated at 9/20/2026, 5:02:08 PM

--- 122-BestTimetoBuyandSellStockII/122-BestTimetoBuyandSellStockII.java
+++ 122-BestTimetoBuyandSellStockII/122-BestTimetoBuyandSellStockII.java
@@ -1,15 +1,20 @@
-// Last updated: 3/2/2026, 3:56:38 PM
+// Last updated: 9/20/2026, 5:02:08 PM
 1class Solution {
 2    public int maxProfit(int[] prices) {
-3        return buy_and_sell_stock(prices);
+3        return answer(prices);
 4    }
-5    public static int buy_and_sell_stock(int[] prices){
-6        int ans=0;
-7        for(int i=0;i<prices.length-1;i++){
-8            if(prices[i+1]>prices[i]){
-9                ans += prices[i+1]-prices[i];
-10            }
-11        }
-12        return ans;
-13    }
-14}
+5
+6    public int answer(int[] arr){
+7        int n = arr.length;
+8        int max_Profit =0;
+9
+10        for(int i=0 ;i<n-1 ;i++){
+11            if(arr[i] < arr[i+1]){
+12                int profit = arr[i+1] - arr[i];
+13                max_Profit += profit;
+14            }
+15        }
+16
+17        return max_Profit;
+18    }
+19}
