79d9d2f1e48f154f636374989500029a4e5ed940
Kuldeep Garg <kuldeepgarg6993@gmail.com>
2026-09-26 15:07:30 +0530

    File updated at 9/26/2026, 3:07:29 PM

--- 881-BoatstoSavePeople/881-BoatstoSavePeople.java
+++ 881-BoatstoSavePeople/881-BoatstoSavePeople.java
@@ -1,25 +1,25 @@
-// Last updated: 10/1/2025, 9:52:02 PM
-class Solution {
-    public int numRescueBoats(int[] people, int limit) {
-        return numberOfBoats(people ,limit);
-    }
-    public static int numberOfBoats(int[] arr , int limit){
-
-        Arrays.sort(arr);
-        int boat =0;
-        int l = 0;
-        int r = arr.length-1;
-        while(l<=r){
-            if(arr[l] + arr[r] <= limit){
-                boat++;
-                l++;
-                r--;
-            }else{
-                boat++;
-                r--;
-            }
-        }
-        return boat;
-
-    }
-}
+// Last updated: 9/26/2026, 3:07:29 PM
+1class Solution {
+2    public int numRescueBoats(int[] people, int limit) {
+3        return numberOfBoats(people ,limit);
+4    }
+5    public static int numberOfBoats(int[] arr , int limit){
+6
+7        Arrays.sort(arr);
+8        int boat =0;
+9        int l = 0;
+10        int r = arr.length-1;
+11        while(l<=r){
+12            if(arr[l] + arr[r] <= limit){
+13                boat++;
+14                l++;
+15                r--;
+16            }else{
+17                boat++;
+18                r--;
+19            }
+20        }
+21        return boat;
+22
+23    }
+24}
